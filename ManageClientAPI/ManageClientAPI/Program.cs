a7421fbc09ce39b164664c3495fc984b35ebb393
makenchi <lucas.santos9322@gmail.com>
2023-08-15 21:08:35 -0300

    feat: injetando dependencias

--- ManageClientAPI/ManageClientAPI/Program.cs
+++ ManageClientAPI/ManageClientAPI/Program.cs
@@ -1,12 +1,24 @@
+using ManageClientAPI.Data;
+using ManageClientAPI.Data.Interfaces;
+using ManageClientAPI.Data.Repos;
+using ManageClientAPI.Services;
+using ManageClientAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IRepositoryCategory,RepositoryCategory>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
