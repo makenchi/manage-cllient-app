33cbda2c7885ba7745552f99c1f97eea331f6bd2
makenchi <lucas.santos9322@gmail.com>
2023-08-17 01:33:06 -0300

    feat: criando lógica da modal

--- manage-clients-web/app/components/AddCategory.tsx
+++ manage-clients-web/app/components/AddCategory.tsx
@@ -5,7 +5,14 @@ import Modal from "./Modal";
 import { useState } from "react";
 
 const AddCategory = () => {
-    const [modalOpen, setModalOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [newCategoryValue, setNewCategoryValue] = useState<string>("");
+
+    const handleSubmitNewCategory: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        console.log(newCategoryValue);
+        setNewCategoryValue("");
+    };
 
     return (
         <div>
@@ -13,7 +20,15 @@ const AddCategory = () => {
                 Add new client category <AiOutlinePlus className="ml-2" size={18} />
             </button>
 
-            <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+            <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+                <form onSubmit={handleSubmitNewCategory}>
+                    <h3 className="font-bold text-lg">Add new category</h3>
+                    <div className="model-action flex flex-row mt-4">
+                        <input value={newCategoryValue} onChange={(e) => setNewCategoryValue(e.target.value)} type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <button type="submit" className="btn ml-4">Submit</button>
+                    </div>
+                </form>
+            </Modal>
         </div>        
     );
 };
--- manage-clients-web/app/components/Modal.tsx
+++ manage-clients-web/app/components/Modal.tsx
@@ -1,16 +1,16 @@
 interface ModalProps{
     modalOpen: boolean
-    setModalOpen: () => void;
+    setModalOpen: (open: boolean) => boolean | void;
+    children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen }) => {
+const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen, children }) => {
     return (
         <dialog id="my_modal_3" className={`modal ${modalOpen ? "modal-open" : ""}`}>
-            <form method="dialog" className="modal-box">
+            <div method="dialog" className="modal-box">
                 <button onClick={() => setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">Press ESC key or click on ✕ button to close</p>
-            </form>
+                {children}
+            </div>
         </dialog>        
     )
 }
