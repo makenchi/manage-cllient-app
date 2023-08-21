4e8a588f6dd23c8ff84a797aeb098446e8bc7c50
makenchi <lucas.santos9322@gmail.com>
2023-08-21 11:37:25 -0300

    fix: configurando entity para utilizar banco in memory

--- ManageClientAPI/ManageClientAPI/Data/DataContext.cs
+++ ManageClientAPI/ManageClientAPI/Data/DataContext.cs
@@ -5,7 +5,11 @@ namespace ManageClientAPI.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {            
+            optionsBuilder.UseInMemoryDatabase(databaseName: "manageclientdb");
+        }
 
         public DbSet<Category> Categories { get; set; }
     }
--- ManageClientAPI/ManageClientAPI/Program.cs
+++ ManageClientAPI/ManageClientAPI/Program.cs
@@ -10,10 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<DataContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors(options => options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
